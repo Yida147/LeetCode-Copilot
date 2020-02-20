f3422a238f18adc986891ea87beb3473039777f1
yida233 <mrxiangyd@outlook.com>
2020-02-21 03:03:34 +0800

    21-MergeTwoSortedLinkedList

---
+++ Yida/LeetCode_21_541.java
@@ -0,0 +1,39 @@
+/**
+ * Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+ *
+ * Example:
+ *
+ * Input: 1->2->4, 1->3->4
+ * Output: 1->1->2->3->4->4
+ */
+public class LeetCode_21_541 {
+    /**
+     * 递归 时间复杂度O(n+m) 空间复杂度O(n+m)
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null) {
+            return l2;
+        }
+        else if (l2 == null) {
+            return l1;
+        }
+        else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+}
